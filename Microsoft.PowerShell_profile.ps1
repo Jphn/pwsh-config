4ba252012d88fff9379ecc25585c1326bf39ff1e
João Pedro Holanda Neves <joaopedroholandaneves@gmail.com>
2022-06-07 20:14:00 -0300

    fixed the last commit

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,32 @@
-oh-my-posh init pwsh --config C:\Users\joaop\AppData\Local\Programs\oh-my-posh\themes\bubblesextra.omp.json | Invoke-Expression
-clear
+# PSReadLine
+Set-PsReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+# ALIASES
+## Functions
+Function GitADD {git add .}
+Function GitCM {
+    param (
+        $m
+    )
+    git commit -m $m
+}
+Function GitS {git status}
+Function GitP {git push}
+
+Function Quit {exit}
+
+## Maps
+Set-Alias -Name !gadd -Value GitADD
+Set-Alias -Name !gs -Value GitS
+Set-Alias -Name !gp -Value GitP
+Set-Alias -Name !gc -Value GitCM
+Set-Alias -Name e -Value explorer.exe
+Set-Alias -Name q -Value Quit
+Set-Alias -Name n -Value nvim
+Set-Alias -Name c -Value composer
+
+# OH MY POSH
+oh-my-posh init pwsh --config C:\Users\joaop\AppData\Local\Programs\oh-my-posh\themes\star.omp.json | Invoke-Expression
+
+# clear
