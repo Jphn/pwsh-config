43e71fd9c06d987562f583c14c577a4f3a7fc5c5
João Pedro Holanda Neves <joaopedroholandaneves@gmail.com>
2022-06-07 20:10:14 -0300

    added more aliases

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,2 @@
-# PSReadLine
-Set-PsReadLineOption -PredictionSource History
-Set-PSReadLineOption -PredictionViewStyle ListView
-
-# ALIASES
-Set-Alias g git
-
-# OH MY POSH
-oh-my-posh init pwsh --config C:\Users\joaop\AppData\Local\Programs\oh-my-posh\themes\star.omp.json | Invoke-Expression
-
+oh-my-posh init pwsh --config C:\Users\joaop\AppData\Local\Programs\oh-my-posh\themes\bubblesextra.omp.json | Invoke-Expression
 clear
